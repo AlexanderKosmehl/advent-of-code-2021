55d1933af7943f7587525d3064c5a0f7bee802b9
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-12-01 23:32:29 +0100

    first commit

---
+++ Day1/Solution.ts
@@ -0,0 +1,51 @@
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+
+/*
+  Helper Functions
+*/
+
+function getNumberOfIncrements(measurements: number[]) {
+  let increments = 0
+  let lastMeasurement: number | undefined = undefined
+
+  for (const measurement of measurements) {
+    // LastIncrement still undefined
+    if (!lastMeasurement) {
+      lastMeasurement = measurement
+      continue
+    }
+
+    // Depth increased -> increase number of increments
+    if (measurement > lastMeasurement) {
+      lastMeasurement = measurement
+      increments++
+      continue
+    }
+
+    // Depth didnt increase
+    lastMeasurement = measurement
+  }
+
+  return increments
+}
+
+/*
+  Solution
+*/
+
+const input = readFileSync(resolve(__dirname, 'Input.txt')).toString()
+const formattedInput = input.split('\r\n').map((entry) => Number(entry))
+
+const increments = getNumberOfIncrements(formattedInput)
+
+const windowInput: number[] = []
+formattedInput.forEach((value, index, array) => {
+  if (index <= array.length - 2)
+    windowInput.push(array[index] + array[index + 1] + array[index + 2])
+})
+
+const windowIncrements = getNumberOfIncrements(windowInput)
+
+console.log('Number of Increments:', increments)
+console.log('Number of Window Increments:', windowIncrements)
